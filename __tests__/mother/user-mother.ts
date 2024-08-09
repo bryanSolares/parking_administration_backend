4a748005eb79a16b40203e6d2ae867b51df2efff
BRYAN JOSUE SOLARES PEREZ <bryan.solares@claro.com.gt>
2024-08-09 17:35:08 -0600

    test: :white_check_mark: Add test for user cases

---
+++ __tests__/mother/user-mother.ts
@@ -0,0 +1,17 @@
+import { faker } from "@faker-js/faker";
+import { UserEntity } from "../../src/auth/domain/entities/user-entity";
+import { UserStatus } from "../../src/auth/domain/entities/user-entity";
+
+export class UserMother{
+  static createUser({
+    id = faker.string.uuid(),
+    name = faker.person.fullName(),
+    email = faker.internet.email(),
+    username = faker.internet.userName(),
+    password = faker.internet.password(),
+    status = "ACTIVO" as UserStatus,
+    phone = faker.phone.number()
+  }): UserEntity {
+    return new UserEntity(id, name, email, username, password, status, phone);
+  }
+}
