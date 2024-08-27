87335b1ba2cea7b6292b92b4ca2ef72394063591
BRYAN JOSUE SOLARES PEREZ <bryan.solares@claro.com.gt>
2024-08-26 18:28:33 -0600

    chore: :label: Update type manage error

--- src/auth/application/use-cases/auth/refresh-token.ts
+++ src/auth/application/use-cases/auth/refresh-token.ts
@@ -1,16 +1,20 @@
 import { AuthRepository } from '@src/auth/domain/repository/auth-repository';
+import { AppError } from '@src/server/config/err/AppError';
 
 export class RefreshTokenUseCase {
   constructor(private readonly authRepository: AuthRepository) {}
 
   async run(username: string) {
     const userEntity = await this.authRepository.findUserByUsername(username);
-    if (!userEntity) throw new Error('User not found');
+
+    if (!userEntity)
+      throw new AppError('REFRESH_TOKEN_USE_CASE', 401, 'User not found', true);
 
     const roleDetail = await this.authRepository.getRoleById(
       userEntity.role as string
     );
-    if (!roleDetail) throw new Error('Role not found');
+    if (!roleDetail)
+      throw new AppError('REFRESH_TOKEN_USE_CASE', 401, 'Role not found', true);
 
     return { user: userEntity, role: roleDetail };
   }
