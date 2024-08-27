a287643872b301132f2a9accdf268c514f89a2b8
BRYAN JOSUE SOLARES PEREZ <bryan.solares@claro.com.gt>
2024-08-26 19:18:40 -0600

    test: :white_check_mark: Add test to auth workflow

--- src/auth/infrastructure/routes/auth.routes.ts
+++ src/auth/infrastructure/routes/auth.routes.ts
@@ -2,10 +2,16 @@ import { Router } from 'express';
 import { authController } from '../dependencies';
 
 import { validateAuth } from '@server/middleware/auth-middleware';
+import { loginSchema } from '../utils/auth-zod-schemas';
+import { validateRequest } from '@shared/zod-validator';
 
 const routes = Router();
 
-routes.post('/login', authController.login.bind(authController));
+routes.post(
+  '/login',
+  validateRequest(loginSchema, 'body'),
+  authController.login.bind(authController)
+);
 routes.post(
   '/refresh-token',
   validateAuth('refresh_token'),
